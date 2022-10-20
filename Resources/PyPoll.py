c8925e3fdaa60a11af1a9de09b3914f9c0a990d7
wsong1985 <114896048+wsong1985@users.noreply.github.com>
2022-10-20 18:51:27 -0400

    Adding the electiuon audit code

--- Resources/PyPoll.py
+++ Resources/PyPoll.py
@@ -40,26 +40,97 @@ print ("The time right now is", now)
 
 
 # Assign a variable for the file to load and the path.
-
 file_to_load = os.path.join("Resources","election_results.csv")
 
-# Open the election results and read the file.
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+
+# Candidate Options
+candidate_options = []
+
+# Declare the empty dictionary.
+candidate_votes = {}
+
+# Winning candidate and winning count tracker
+
+winning_candidate = ""
+
+winning_count = 0
+
+winning_percentage = 0
+
+
+# Open the election results and read the file.
 with open(file_to_load) as election_data:
 
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
-
-    # # Print each row in the CSV file.
-    # for row in file_reader:
-    #     print(row)
     
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)    
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes +=1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        #  If the candidate does not match any existing candidate..
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count/
+        candidate_votes[candidate_name] +=1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    # Print the candidate name and percentage of votes.
+    #print(f"{candidate_name}: received {vote_percentage}% of the vote.")
+
+    # To do: print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    # Determine winning vote count and candidate
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+# print(total_votes)
+# print(candidate_votes)
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
+
 
 # Using the open() function with the "w" mode we will write data to the file.
 open(file_to_save, "w")
